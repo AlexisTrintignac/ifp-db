803f1690a1e6feddfb383adf85e22bda7ea00a72
Alexis Trintignac <alexis.trintignac@gmail.com>
2020-04-08 14:18:37 +0200

    add connect derby with hibernate

--- src/Domain/Models/HibernateBasicTest.java
+++ src/Domain/Models/HibernateBasicTest.java
@@ -1,4 +1,6 @@
 package Domain.Models;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,13 @@ class HibernateBasicTest {
 
     @Test
     void sessionOk() {
-        Persistence.createEntityManagerFactory("derby-persistence-unit");
+        Reporter reporterDB = new Reporter();
+        EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("derby-persistence-unit");
+        EntityManager entityManager = sessionFactory.createEntityManager();
+        assertTrue(entityManager.isOpen());
+        entityManager.persist(reporterDB);
+        reporterDB.setCredit(2);
+        entityManager.close();
+        sessionFactory.close();
     }
 }
--- src/Domain/Models/Reporter.java
+++ src/Domain/Models/Reporter.java
@@ -1,8 +1,45 @@
 package Domain.Models;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "REPORTER")
 public class Reporter {
 
-    private String pseudo;
-    private int credit;
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
     private int id;
+
+    @Column(name = "PSEUDO")
+    private String pseudo;
+
+    @Column(name = "CREDIT")
+    private int credit;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
 }
